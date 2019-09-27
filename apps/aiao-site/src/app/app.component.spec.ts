21e49486fd0e69ba74479a8e3d702a8cf6dfdb63
Jimmy Liu <hero63418@gmail.com>
2019-09-27 10:26:05 +0800

    add ci

--- apps/aiao-site/src/app/app.component.spec.ts
+++ apps/aiao-site/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -24,8 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to aiao-site!'
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to aiao-site!');
   });
 });
